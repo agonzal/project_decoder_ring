86f195e0de5c218df4ba1f63088be14b570277ac
Albert Gonzalez <albertg@cerveau.us>
2021-01-21 23:34:59 -0800

    refactor caesar.js one final time.

--- src/caesar.js
+++ src/caesar.js
@@ -1,58 +1,26 @@
-
-const alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
-
 function caesar(input, shift, encode = true) {
-  
-  // validate parameters. 
-
   if (!shift || shift > 25 || shift < -25 || shift === 0) return false;
-  
 
-  let result = [];
+  if (!encode) shift = 0 - shift; // set initial shift
 
-  if (!encode) shift = 0 - shift; // set initial shift. 
+  const alphabetArr = "abcdefghijklmnopqrstuvwxyz".split("");
+  let res = "";
 
-  const words = input.toLowerCase().split(' '); // iterate over input, split string into individual words.  
-  
+  for (let i = 0; i < input.length; i++) {
+    const char = input[i].toLowerCase();
+    const idx = alphabetArr.indexOf(char);
 
-  for (let word in words) { // iterate over [words] 
-    
-    const currentWord = words[word];
-    let emptyStr = '';
-
-    for (let letter in currentWord) { // iterate each word to retrieve each character. 
-     
-      const current0 = currentWord[letter];
-
-      if (current0.match(/([a-z])/g)) {   // is character an alphabet letter (a to z)
-
-      let charIndex = alphabet.indexOf(current0) + shift; // use index location to find corresponding substitution character. 
-
-      if (charIndex > 25) { 
-        emptyStr += alphabet[charIndex - 26];      // if true subtract 26. 
-      } else if (charIndex < 0) {   
-        emptyStr += alphabet[charIndex + 26];      // Add 26 when false. 
-      } else {
-        emptyStr += alphabet[charIndex];
-      }
-    } else {                                       //  if it isnt an alphabet character, add it to emptyStr. 
-      
-      emptyStr += current0;
-    
+    // if it is not a letter, don`t shift it
+    if (idx === -1) {
+      res += char;
+      continue;
     }
 
-        
-    } 
-   
-    result.push(emptyStr);    // push character at that index point to result array.
-    
+    // only 26 letters in alphabet, if > 26 it wraps around
+    const encodedIdx = (idx + shift) % 26;
+    res += alphabetArr[encodedIdx];
   }
-  
-  return result.join(' ');   // return the final result adding whitespaces. 
+  return res;
 }
 
 module.exports = caesar;
-
-
-
-
